952df8336cfd4b9ff6248a7c697ec0aa607d602a
codeferal <codeferal@protonmail.com>
2024-02-14 03:57:45 +0200

    reviews patch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import PitchGroup from "./components/pitch/PitchGroup";
+import ReviewGroup from "./components/reviews/ReviewGroup";
 import ServiceGroup from "./components/services/ServiceGroup";
 
 import "./styles/reset.css";
 import "./styles/style.css";
 
+// TODO: the cool tooth idea
+// I think the reveal canvas stuff
+// Otherwise have to go the long way
+
 function App() {
   return (
     <>
+      <ReviewGroup />
       <PitchGroup />
       <ServiceGroup />
     </>
---
+++ src/components/reviews/Review.tsx
@@ -0,0 +1,67 @@
+import { Show } from "solid-js";
+import styles from "./Review.module.css";
+
+interface ReviewProps {
+  picture: string;
+  name: string;
+  review: string;
+  link?: string;
+}
+
+export default function Review(props: ReviewProps) {
+  return (
+    <>
+      <div class={styles.container}>
+        <div class={styles["picture-container"]}>
+          <img class={styles.picture} src={props.picture} alt={props.name} />
+        </div>
+
+        <div class={styles["visible-container"]}>
+          <div class={styles.content}>
+            <div class={styles.contact}>
+              <p class={styles.name}>{props.name}</p>
+
+              <Show when={props.link}>
+                <div class={styles.link}>
+                  <a href={props.link} target="_blank">
+                    <img src="../../../public/link.svg" alt="link" />
+                  </a>
+                </div>
+              </Show>
+            </div>
+
+            <div class={styles.stars}>
+              <img
+                class={styles.star}
+                src="../../../public/star.svg"
+                alt="star"
+              />
+              <img
+                class={styles.star}
+                src="../../../public/star.svg"
+                alt="star"
+              />
+              <img
+                class={styles.star}
+                src="../../../public/star.svg"
+                alt="star"
+              />
+              <img
+                class={styles.star}
+                src="../../../public/star.svg"
+                alt="star"
+              />
+              <img
+                class={styles.star}
+                src="../../../public/star.svg"
+                alt="star"
+              />
+            </div>
+
+            <p class={styles.review}>{props.review}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
