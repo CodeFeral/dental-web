99409b82dbc12e5b6b1664714205ba3b0c4f000e
codeferal <codeferal@protonmail.com>
2024-02-14 16:42:05 +0200

    pitch patch

--- src/components/pitch/PitchGroup.tsx
+++ src/components/pitch/PitchGroup.tsx
@@ -5,19 +5,19 @@ export default function PitchGroup() {
   return (
     <div class={styles.container}>
       <Pitch
-        image="../../../public/pitch/display-teeth-svgrepo-com.svg"
-        title="Modern Approach"
-        description="Forget old fashioned dentistry – welcome to the future. The Dental by Design experience makes it as seamless and convenient as possible for you. Our unique and high-tech approach not only limits your anxiety level but also gets you same day services and saves you time."
+        image="../../../public/pitch/doctor.svg"
+        title="Individualization"
+        description="Dentistry demands a personalized approach; no two smiles are alike. I aim for perfection, customizing your care to fit your unique needs. Throughout your treatment, I focus on education, ensuring you leave with not just a healthier smile, but also increased confidence in your dental health."
       />
       <Pitch
         image="../../../public/pitch/tooth-green-svgrepo-com.svg"
         title="Anxiety Free"
-        description="At my clinic, I try to do my best to make you feel comfortable and at ease. From our spa like feel to our professional individualized services, our intent is to change the way you approach dentistry. Relax unwind and make sure to enjoy every step of your experience."
+        description="At my clinic, my priority is to ensure your comfort and peace of mind. Moving away from the conventional dental experience, I've created an environment where stress-free care is the norm. My approach is personal and tailored to your individual needs, ensuring every aspect of your visit is focused on making you feel relaxed."
       />
       <Pitch
-        image="../../../public/pitch/doctor.svg"
-        title="Individualization"
-        description="There is no one-size-fits-all approach to dentistry. Expect perfection and a personalized experience at Dental by Design. Our data collection process is highly detailed and we offer patient education throughout your treatment so you leave feeling more confident about your dental care."
+        image="../../../public/pitch/display-teeth-svgrepo-com.svg"
+        title="Modern Approach"
+        description="Step into the future of dentistry, where tradition meets innovation. My practice is designed to make your experience as smooth and convenient as possible. With a unique, high-tech approach, I focus on minimizing your anxiety and providing efficient services that save you time, often with same-day results."
       />
     </div>
   );
