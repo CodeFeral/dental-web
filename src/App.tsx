13c26c726930616587efa31ef05d70dcc3a90e8d
codeferal <codeferal@protonmail.com>
2024-02-14 15:07:04 +0200

    form patch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import PitchGroup from "./components/pitch/PitchGroup";
+import Form from "./components/form/Form";
 import ReviewGroup from "./components/reviews/ReviewGroup";
+import PitchGroup from "./components/pitch/PitchGroup";
 import ServiceGroup from "./components/services/ServiceGroup";
 
 import "./styles/reset.css";
@@ -8,10 +9,14 @@ import "./styles/style.css";
 // TODO: the cool tooth idea
 // I think the reveal canvas stuff
 // Otherwise have to go the long way
+//
+// XP: 5 years
+// clients: 3k
 
 function App() {
   return (
     <>
+      <Form />
       <ReviewGroup />
       <PitchGroup />
       <ServiceGroup />
---
+++ src/components/form/Form.tsx
@@ -0,0 +1,65 @@
+import styles from "./Form.module.css";
+
+export default function Form() {
+  return (
+    <div class={styles.container}>
+      <form
+        class={styles.form}
+        method="post"
+        action="https://www1.domain.com/scripts/formemail.html"
+      >
+        <div class={styles.content}>
+          <input type="hidden" name="my_email" value="doniadept@gmail.com" />
+
+          <div class={styles.group}>
+            <input
+              class={styles.input}
+              type="text"
+              name="fullname"
+              size="25"
+              placeholder="Name"
+              required
+            />
+          </div>
+
+          <div class={styles.group}>
+            <input
+              class={styles.input}
+              type="email"
+              name="email"
+              size="25"
+              placeholder="Email"
+              required
+            />
+          </div>
+
+          <div class={styles.group}>
+            <textarea
+              class={styles.textarea}
+              name="message"
+              placeholder="Type your message here..."
+              required
+            ></textarea>
+          </div>
+
+          <input
+            type="hidden"
+            name="subject"
+            value="form submission from %%fullname%%"
+          />
+
+          <input type="hidden" name="required" value="email, fullname" />
+
+          <div class={`${styles["button-container"]} ${styles.group}`}>
+            <input
+              class={styles.button}
+              type="submit"
+              name="submit"
+              value="Submit Now"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
