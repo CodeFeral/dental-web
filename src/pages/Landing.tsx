03a0eb10e683d7da05b8491056dd887e20840225
codeferal <codeferal@protonmail.com>
2024-02-17 03:56:55 +0200

    landing patch

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,11 +4,26 @@ export default function Landing() {
   return (
     <section class={styles.container}>
       <div class={styles.content}>
-        <div class={styles.welcome}>
-          <h1>Welcome!</h1>
-          <h2>Top kek!</h2>
+        <div class={styles.group}>
+          <div class={styles.welcome}>
+            <h1 class="bold">Dedicated to<br/>Dental Excellence</h1>
+            <p>
+              Lorem ipsum dolor sit amet, qui minim labore adipisicing minim
+              sint.
+            </p>
+            <button class="button">Book Now</button>
+          </div>
         </div>
-        <div class={styles.visual}></div>
+
+        <div class={styles.group}>
+          <div class={styles.visual}>
+            <img
+              class={styles.image}
+              src="../../public/tooth.svg"
+              alt="tooth"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
