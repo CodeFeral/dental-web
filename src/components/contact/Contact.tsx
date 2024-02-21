81d95f43b3f150bcbc9be299f248d9a89732cd6e
codeferal <codeferal@protonmail.com>
2024-02-21 20:04:29 +0200

    cleanup; css; minor

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -7,30 +7,24 @@ export default function Contact() {
       <ContactRow
         title="Phone Number"
         image="../../../public/contact/phone.svg"
-        imageAlt="phone"
         description="+359 885 691 021"
         href="tel:0885691021"
       />
       <ContactRow
         title="Email"
         image="../../../public/contact/email.svg"
-        imageAlt="email"
         description="g.bagdatov@gmail.com"
         href="mailto:g.bagdatov@gmail.com"
-        target
       />
       <ContactRow
         title="Clinic Address"
         image="../../../public/contact/location.svg"
-        imageAlt="location"
         description='ul. "Biser" 2 g.k. Lozenets, Sofia'
         href="https://maps.app.goo.gl/AbfHVwvSV4ob8fFy5"
-        target
       />
       <ContactRow
         title="Open Hours"
         image="../../../public/contact/clock.svg"
-        imageAlt="clock"
         description="09:00 - 21:00"
       />
     </div>
--- src/components/contact/ContactRow.tsx
+++ src/components/contact/ContactRow.tsx
@@ -1,63 +1,27 @@
-import { Show, createSignal } from "solid-js";
+import { Show } from "solid-js";
 import styles from "./ContactRow.module.css";
 
 interface ContactRowProps {
   title: string;
   image: string;
-  imageAlert?: string;
-  imageAlt: string;
   description: string;
-  emergency?: boolean;
   href?: string;
-  target?: boolean;
 }
 
 export default function ContactRow(props: ContactRowProps) {
-  const [hover, setHover] = createSignal(false);
-
-  const imgSrc = () =>
-    props.emergency && props.href && hover() ? props.imageAlert : props.image;
-  const linkTarget = props.target ? "_blank" : undefined;
-
   return (
     <div class={styles.container}>
-      <img
-        class={`${styles.image} disable-selection`}
-        src={imgSrc()}
-        alt={props.imageAlt}
-      />
+      <img class={`${styles.image} disable-selection`} src={props.image} />
 
       <div class={styles.content}>
         <p class={`${styles.title} disable-selection`}>{props.title}</p>
 
         <Show
-          fallback={
-            <p
-              classList={{
-                [styles.description]: true,
-                [styles.emergencyColor]: props.emergency,
-              }}
-            >
-              {props.description}
-            </p>
-          }
           when={props.href}
+          fallback={<p class={styles.description}>{props.description}</p>}
         >
-          <a
-            href={props.href}
-            onmouseenter={() => setHover(true)}
-            onmouseleave={() => setHover(false)}
-            target={linkTarget}
-          >
-            <p
-              classList={{
-                [styles.description]: true,
-                ["color-orange"]: hover() || props.emergency,
-                ["color-red"]: hover() && props.emergency,
-              }}
-            >
-              {props.description}
-            </p>
+          <a class={styles.link} href={props.href} target="_blank">
+            <p class={styles.description}>{props.description}</p>
           </a>
         </Show>
       </div>
